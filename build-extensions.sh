df0e0fbd1e394fcdc52be56214ab1125b1a45efb
Jon Gunderson <jongund@illinois.edu>
2026-07-02 13:39:29 -0500

    udpated how extension distribution files a built

--- build-extensions.sh
+++ build-extensions.sh
@@ -3,8 +3,31 @@ echo "Building extensions.."
 
 version="2.0.1"
 
-zip -r ./docs/dist/h2l-side-panel-firefox-$version.zip extension-firefox/*  -x ".*" -x "__MACOSX"
-zip -r ./docs/dist/h2l-side-panel-chrome-$version.zip  extension-chrome/*   -x ".*" -x "__MACOSX"
-zip -r ./docs/dist/h2l-side-panel-edge-$version.zip    extension-edge/*     -x ".*" -x "__MACOSX"
-crx3 extension-opera -p ../pem/opera-h2l.pem -o ./docs/dist/h2l-side-panel-opera-$version.crx
+echo "
+Building H2L browser extenions
 
+Creating Firefox extension zip file ...
+"
+cd extension-firefox
+rm ../docs/dist/h2l-side-panel-firefox-$version.zip
+zip -r ../docs/dist/h2l-side-panel-firefox-$version.zip . -x ".*" -x "__MACOSX"
+
+echo "
+Creating Edge extension zip file ...
+"
+cd ../extension-edge
+rm ../docs/dist/h2l-side-panel-edge-$version.zip
+zip -r ../docs/dist/h2l-side-panel-edge-$version.zip    . -x ".*" -x "__MACOSX"
+
+echo "
+Creating Chrome extension zip file ...
+"
+cd ../extension-chrome
+rm ../docs/dist/h2l-side-panel-chrome-$version.zip
+zip -r ../docs/dist/h2l-side-panel-chrome-$version.zip  . -x ".*" -x "__MACOSX"
+
+echo "
+Creating Opera extension crx file ...
+"
+cd ../extension-opera
+crx3 . -p ../../pem/opera-h2l.pem -o ../docs/dist/h2l-side-panel-opera-$version.crx
